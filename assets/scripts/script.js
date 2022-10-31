6779ddfefad294370e7ae8de27ef10097a94a90f
Leah C <101465290+Leah9@users.noreply.github.com>
2022-10-31 19:00:05 +0000

    Display correct number of underscores on screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,16 @@ let difficulty = 'Easy'; // Difficulty level
 let lettersTried = []; // Array of letters that have been tried
 let attemptsRemaining = 15; // Guesses remaining
 let lettersCorrect = 0; // How manu letters have been guessed correctly
+let wordDisplay = []; // Array for the guessed letters progress display
 
 // Choose a random word from the dictEasy array
 let word = dictEasy[Math.floor(Math.random() * dictEasy.length)];
 console.log(word); // Check that a word is chosen
+
+// Create correct number of underscores in display
+for (let i = 0; i < word.length; i++) {
+    wordDisplay.push('_')
+}
+console.log(wordDisplay);
+
+$('#word-display').text(wordDisplay); // Update display
