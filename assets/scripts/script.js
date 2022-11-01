e2d6981176d45be7e4d22da8825ffacce0dc8e25
Leah C <101465290+Leah9@users.noreply.github.com>
2022-11-01 14:39:08 +0000

    Refactor code into smaller functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,38 @@
 // Create an array of easy level words
-let dictEasy = ['CAT', 'DOG', 'BALL', 'FOOT', 'HAND', 'HEAD', 'RAT', 'BAG', 'SAD', 'PAT', 'LEG', 'ARM', 'CAR', 'BUS', 'TRAIN'];
-
+const dictEasy = ['CAT', 'DOG', 'BALL', 'FOOT', 'HAND', 'HEAD', 'RAT', 'BAG', 'SAD', 'PAT', 'LEG', 'ARM', 'CAR', 'BUS', 'TRAIN'];
 // Create an array to contain our alphabet
-let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 let difficulty = 'Easy'; // Difficulty level
 let lettersTried = []; // Array of letters that have been tried
 let attemptsRemaining = 15; // Guesses remaining
 let lettersCorrect = 0; // How manu letters have been guessed correctly
-let wordDisplay = []; // Array for the guessed letters progress display
+var wordDisplay = []; // Array for the guessed letters progress display
+let word = [];
 
-// Choose a random word from the dictEasy array
-let word = dictEasy[Math.floor(Math.random() * dictEasy.length)];
-console.log(word); // Check that a word is chosen
+function resetGameVariables() {
+    let lettersTried = []; // Array of letters that have been tried
+    let attemptsRemaining = 15; // Guesses remaining
+    let lettersCorrect = 0; // How manu letters have been guessed correctly
+    var wordDisplay = [];
+}
 
-// Updates the on screen display with the guessed letters
-function updateWordDisplay() {
+function chooseWord() {
+    // Choose a random word from the dictEasy array
+    word = dictEasy[Math.floor(Math.random() * dictEasy.length)];
+    console.log(word); // Check that a word is chosen
+    // Create correct number of underscores in display
+    for (let i = 0; i < word.length; i++) {
+        wordDisplay.push('_')
+    }
+}
+
+function updateWordDisplay() { // Updates the on screen display with the guessed letters
     $('#word-display').text('');
     for (letter in wordDisplay) {
         $('#word-display').append(wordDisplay[letter]);
     }
     lettersCorrect++;
-}
-
-// Create correct number of underscores in display
-for (let i = 0; i < word.length; i++) {
-    wordDisplay.push('_')
-}
-console.log(wordDisplay);
-
-$('#word-display').text(wordDisplay); // Update display
-$('#guesses-remaining').text("Attempts remaining = " + attemptsRemaining);
-
-// Create on screen keyboard
-for (let i = 0; i < alphabet.length; i++) {
-    $('#keyboard').append(`<div id="${alphabet[i]}" class="keyboard">${alphabet[i]}</div>`);
 }
 
 function checkLetterAgainstWord(guessedLetter) {
@@ -48,17 +46,26 @@ function checkLetterAgainstWord(guessedLetter) {
         } else {
             $('#' + guessedLetter).addClass('red');
         }
-        lettersTried.push(this.id);
-        attemptsRemaining--;
-        $('#guesses-remaining').text("Attempts remaining = " + attemptsRemaining);
-        if (lettersCorrect === word.length) {
-            alert("Well done!")
-        } else if (attemptsRemaining === 0) {
-            alert("Game over");
-        }
     }
-
+    lettersTried.push(this.id);
+    attemptsRemaining--;
+    $('#guesses-remaining').text("Attempts remaining = " + attemptsRemaining);
+    if (lettersCorrect === word.length) {
+        alert("Well done!")
+    } else if (attemptsRemaining === 0) {
+        alert("Game over");
+    }
 }
+
+chooseWord();
+console.log(wordDisplay);
+$('#word-display').text(wordDisplay); // Update display
+$('#guesses-remaining').text("Attempts remaining = " + attemptsRemaining);
+// Create on screen keyboard
+for (let i = 0; i < alphabet.length; i++) {
+    $('#keyboard').append(`<div id="${alphabet[i]}" class="keyboard">${alphabet[i]}</div>`);
+}
+
 
 // Called when the on screen keyboard is pressed
 $('.keyboard').click(function () {
